608f5e171cd6375ecc03441c26f20d89d7c2fb87
Bimo Wahyu Syahputro <128451452+bimowahyu@users.noreply.github.com>
2024-10-20 01:34:14 +0700

    Update UserModel.js

--- model/UserModel.js
+++ model/UserModel.js
@@ -2,7 +2,6 @@ import { Sequelize } from "sequelize";
 import db from "../config/database.js";
 
 const {DataTypes} = Sequelize;
-
 const Users = db.define('users',{
     uuid:{
         type: DataTypes.STRING,
